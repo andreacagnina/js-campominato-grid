c8384766a73336083de5b976f93e38742f26c02c
andreacagnina <amirkal@libero.it>
2024-07-05 17:57:21 +0200

    [feat] rewind all

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
-document.getElementById('btn_start').addEventListener('click', function () {
+let container = document.getElementsByClassName('container')
+console.log(container)
 
-
-    for (let i = 1; i < 101; i++) {
-
-        let row = document.querySelector('.row');
-        let col = document.createElement('div');
-        let cont = document.createElement('div');
-
-        col.className = 'col-1';
-
-
-        cont.className += 'content';
-
-
-        console.log(col)
-        row.append(col);
-        col.append(cont);
-
-        cont.innerText = (i)
-
-
-    }
-});
+function createSquare() {
+    let currentElement = document.createElement('div')
+}
