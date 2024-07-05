e047c6d03a1e9076d063b3801d2e08b3f877d504
andreacagnina <amirkal@libero.it>
2024-07-05 17:10:50 +0200

    [feat] innertext 1. salvataggio pre ticket

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 document.getElementById('btn_start').addEventListener('click', function () {
 
-    let col = document.createElement('div');
-    let cont = document.createElement('div');
+    for (let i = 1; i < 100; i++) {
 
-    col.className = 'col-1';
-    cont.className += 'content border border-black d-flex justify-content-center align-items-center';
+        let row = document.querySelector('.row');
+        let col = document.createElement('div');
+        let cont = document.createElement('div');
 
-    console.log(col)
+        col.className = 'col-1';
 
-    col.append(cont);
+        cont.className += 'content border border-black d-flex justify-content-center align-items-center bg-dark w-100 my-2 ratio ratio-1x1 text-white';
 
+
+        console.log(col)
+        row.append(col);
+        col.append(cont);
+
+        cont.innerText = (i)
+
+        let first = document.querySelector('.col-1');
+        first.className += ' offset-2';
+
+    }
 });
